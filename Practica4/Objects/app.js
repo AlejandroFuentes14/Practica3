cf33313715db9b30e3d539aad60358cf19143e0a
AlejandroFuentes14 <90477678+AlejandroFuentes14@users.noreply.github.com>
2023-02-15 19:59:23 -0600

    Practica 4 lista

--- Practica4/Objects/app.js
+++ Practica4/Objects/app.js
@@ -1,1 +1,23 @@
+let persona = {
+    nombre: "Jesús",
+    apellido: "Fuentes",
 
+    getName: function (){ //Esta función nos ayuda a completar una oración directamente debido a la concatenación y llamada de variables clave.
+        return `${this.nombre} ${this.apellido}`
+    },
+
+    domicilio: {
+        calle: "Madero #144",
+        colonia: "centro",
+        cp: 28000,
+        municipio: "Colima",
+
+        getDomicilio: function(){ //Aquí tienes que tener cuidado donde vas a escribir el código porque está dentro de la capa de persona y la capa de inicio, es como cavar en el código.
+            return `${this.calle} colonia: ${this.colonia} CP: ${this.cp}, ${this.municipio}`
+        }
+    },
+
+}
+
+console.log(persona.getName());//Aquí imprimos getName
+console.log(persona.domicilio.getDomicilio());// Aquí imprimimos getDomicilio
