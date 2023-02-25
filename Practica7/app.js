c347f50b34968af7e4ae86c759d260c9a29b5e03
AlejandroFuentes14 <90477678+AlejandroFuentes14@users.noreply.github.com>
2023-02-25 14:59:52 -0600

    Add files via upload

--- Practica7/app.js
+++ Practica7/app.js
@@ -1,1 +1,22 @@
+// var Emitter = require("./emitter");
+var Emitter = require("events"); 
 
+let config = require("./config");
+
+emtr.on("greet", () => {
+    console.log("Somewhere, someone said hello. ");
+})
+
+emtr.on(config.events.GREET,() => {
+    console.log("A greeting ocurred!");
+})
+
+console.log("Hello!");
+emtr.emit("greet");
+
+emtr.on(config.events.JUMP, () => {
+    console.log("someone jumped!");
+})
+
+console.log(emtr);
+emtr.emit("jump");
