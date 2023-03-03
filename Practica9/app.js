fc18ee49b6c1982778c54a9610d6b8f0851c985c
AlejandroFuentes14 <90477678+AlejandroFuentes14@users.noreply.github.com>
2023-03-02 21:01:16 -0600

    Practica9 lista

--- Practica9/app.js
+++ Practica9/app.js
@@ -1,1 +1,27 @@
+const express = require('express'); //Se importa el express
+const app = express(); // Creamos una instancia 
 
+app.get('/', (req, res) => { // Creamos una ruta con get y html
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1> Hello World!</h1>
+    </body>
+    </html>`);
+});
+
+app.get('/uno', (req, res) => { 
+    res.send('Hello world, from route one');
+});
+
+app.get('/prueba', (req, res) => {
+    res.send('Hello world, from route prueba');
+});
+
+app.listen(3000); 
