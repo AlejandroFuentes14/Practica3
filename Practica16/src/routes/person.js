fac613e1f139c3df401d5a3f81b632d2c5fbd6bf
AlejandroFuentes14 <90477678+AlejandroFuentes14@users.noreply.github.com>
2023-03-30 22:03:15 -0600

    Add files via upload

--- Practica16/src/routes/person.js
+++ Practica16/src/routes/person.js
@@ -1,1 +1,9 @@
+let express = require('express');
 
+let router = express.Router();
+
+router.get('/person', (req, res) => {
+    res.send('haz solicitado un listado de personas');
+});
+
+module.exports = router;
