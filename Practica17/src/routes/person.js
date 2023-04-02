291ffb2b8a2558f22684b701d744a625575bbbbe
AlejandroFuentes14 <90477678+AlejandroFuentes14@users.noreply.github.com>
2023-04-01 23:36:08 -0600

    Add files via upload

---
+++ Practica17/src/routes/person.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+let router = express.Router();
+
+router.get('/student', (req, res) => {
+    let t = req.params.id; 
+    res.render('student'); 
+});
+
+router.post('/addStudent', (req, res) => {
+    res.render('displayStudent', {
+        nombre: req.body.nombre, 
+        edad: req.body.edad,
+        nss: req.body.nss,
+        tipoSangre: req.body.tipoSangre,
+    });
+});
+
+router.post('/personJson', express.json({ type: '*/*' }), (req, res) => {
+    console.log(`nombre: ${req.body.nombre}
+    apellido: ${req.body.apellido}`);
+});
+
+router.get('/testJson', (req, res) => {
+    res.render('testJson');
+});
+
+module.exports = router;
